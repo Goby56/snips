fb36e5fa773c623c38062de44449d1de2b99f380
Goby56 <60710855+Goby56@users.noreply.github.com>
2023-04-29 00:39:39 +0200

    highlighting
    need to fix post not centered

---
+++ static/js/submit.js
@@ -0,0 +1,55 @@
+function serializeObject(form) {
+    let obj = {}
+    for (let kv of form.serializeArray()) {
+        obj[kv["name"]] = kv["value"]
+    }
+    return obj
+}
+
+hljs.listLanguages().forEach(lang => {
+    $("#language-dropdown").append(`<option value="${lang}">${lang}</option>`)
+})
+
+function renderPreview(title, code, desc, lang) {
+    if (!title) title = "I am bad with titles"
+    if (lang != "auto") {
+        lang = "language-" + lang
+    } else {
+        lang = ""
+    }
+    if (desc) desc = `<p class="description">${desc}</p>`
+    $("#submit-container").append(`
+        <div id="snippet-preview">
+            <div class="post">
+                <p class="title">${title}</p>
+                <div class="snippet">
+                    <pre><code class="snippet-container ${lang}">
+                    </code></pre>
+                    ${desc}
+                </div>
+            </div>  
+        </div>`)
+    $(".snippet-container").text(code)
+    hljs.highlightAll();
+}
+
+$("#preview-button").on("click", e => {
+    if ($(e.currentTarget).text() == "Preview") {
+        let data = serializeObject($("#editor-form"))
+
+        let selectedLanguage = $("#language-dropdown").find(":selected").val()
+        renderPreview(data.title, data.snippet, data.description, selectedLanguage)
+
+        $("#snippet-editor").addClass("hide")
+        $("#language-dropdown").addClass("hide")
+
+        $(e.currentTarget).text("Edit")
+    } else {
+        $("#snippet-preview").remove()
+
+        $("#snippet-editor").removeClass("hide")
+        $("#language-dropdown").removeClass("hide")
+
+        $(e.currentTarget).text("Preview")
+    }
+})
